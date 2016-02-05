37c6c66bbe85213b4f161bf4579bf23113085f84
Metel Patel <metelpatel@gmail.com>
2016-02-05 11:43:55 -0600

    modals work

--- starter-code/app/public/javascripts/app.js
+++ starter-code/app/public/javascripts/app.js
@@ -9,8 +9,8 @@ $(function() {
   })
   .done(function(data){
     for (var i = 0; i < data.length; i++){
-      $('#doughnuts').append("<li>" + data[i].flavor + " " + data[i].style + "</li>");
-    }
+      $('#doughnuts').append('<li><a href="#" data-toggle="modal" data-target="#' + data[i].id + '">' + data[i].flavor + " " + data[i].style + '</li>');
+      $('#doughnuts').append('<div id="' + data[i].id + '" class="modal fade" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">'+ data[i].id + '</h4></div><div class="modal-body"><p>Some text in the modal.</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>');    }
     console.log(data);
   })
   .fail(function(jqXHR, textStatus) {
@@ -47,7 +47,10 @@ $(function() {
      });
 
      return false;
+
    });
+
+
 
 
 
