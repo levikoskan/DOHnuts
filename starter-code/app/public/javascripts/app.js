253d115167c4429043abcb7b3bbb0f7cc9e4d238
Metel Patel <metelpatel@gmail.com>
2016-02-05 10:03:39 -0600

    displaying dohnuts

--- starter-code/app/public/javascripts/app.js
+++ starter-code/app/public/javascripts/app.js
@@ -1,4 +1,24 @@
 // jQuery Document Ready
 $(function() {
-  
+  var donutApi = "http://api.doughnuts.ga/doughnuts";
+  var jqXHR = $.ajax({
+    method: "GET",
+    url: donutApi,
+    data: {},
+    dataType: "json"
+  })
+  .done(function(data){
+    for (var i = 0; i < data.length; i++){
+      $('#doughnuts').append("<li>" + data[i].flavor + " " + data[i].style + "</li>");
+    }
+    console.log(data);
+  })
+  .fail(function(jqXHR, textStatus) {
+     console.log("Request failed: " + textStatus);
+  })
+  .always(function() {
+     console.log('Request completed');
+       //hideLoadingIndicator();
+  });
+
 });
